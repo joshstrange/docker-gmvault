d4a86323895e1747fe1803c53e8390991fa85ac8
Josh Strange <joshstrange@users.noreply.github.com>
2015-07-17 21:24:24 -0400

    Setting paths to always be the same

--- gmvault.sh
+++ gmvault.sh
@@ -1,18 +1,2 @@
 #!/usr/bin/env bash
-
-db_dir="${DB_DIR}"
-
-if [ ! $db_dir ] || [ ! -d $db_dir ] ; then
-  echo ""
-  echo "***************************************"
-  echo "must set DB_DIR environment variable, and dir must exist on container"
-  echo "***************************************"
-  echo ""
-
-  echo "EXAMPLE:"
-  echo "docker run -e DB_DIR=/mnt/storage/email-backup -i -t narf/gmvault sync your@gmail.com"
-  echo ""
-  exit 1
-fi
-
-gmvault $* --db-dir $db_dir
+gmvault $* --db-dir /db -d /backup
